1eef44a12ee3841c8da33b0e8a3cb2729a016dd4
Noor elhoda ahmed <nur@elephantapps.co>
2024-03-05 19:37:50 +0200

    Initial Commit

---
+++ CamraFrameWork/CamraFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  CamraFrameWork.h
+//  CamraFrameWork
+//
+//  Created by norelhoda on 05/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CamraFrameWork.
+FOUNDATION_EXPORT double CamraFrameWorkVersionNumber;
+
+//! Project version string for CamraFrameWork.
+FOUNDATION_EXPORT const unsigned char CamraFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CamraFrameWork/PublicHeader.h>
+
+
